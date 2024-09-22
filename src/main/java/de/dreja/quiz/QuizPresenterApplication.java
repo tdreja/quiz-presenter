cfabccc6d9af2c536185fbdf3a00ddd6d2a4ed63
Thomas Dreja <thomas.dreja@gmail.com>
2024-09-22 16:11:27 +0200

    chore(*): Initialize SpringBoot project

---
+++ src/main/java/de/dreja/quiz/QuizPresenterApplication.java
@@ -0,0 +1,13 @@
+package de.dreja.quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizPresenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizPresenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/dreja/quiz/QuizPresenterApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dreja.quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizPresenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
