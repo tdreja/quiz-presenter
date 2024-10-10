29d9615a9f9148eb3db4b957afa201c39b3a518c
Thomas Dreja <thomas.dreja@exxcellent.de>
2024-10-10 15:55:51 +0200

    feat(Game): Add test iframe

--- src/main/java/de/dreja/quiz/interfaces/web/GameEndpoint.java
+++ src/main/java/de/dreja/quiz/interfaces/web/GameEndpoint.java
@@ -1,10 +1,12 @@
 package de.dreja.quiz.interfaces.web;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @Controller
@@ -14,5 +16,18 @@ public class GameEndpoint {
     public String getMainPage(Model model) {
         model.addAttribute("now", LocalDate.now());
         return "index";
+    }
+
+    @GetMapping("/game/{gameId}")
+    public String getGameOuterPage(@PathVariable("gameId") String gameId, Model model) {
+        model.addAttribute("gameId", gameId);
+        return "game";
+    }
+
+    @GetMapping("/game/{gameId}/content")
+    public String getGameInnerPage(@PathVariable("gameId") String gameId, Model model) {
+        model.addAttribute("gameId", gameId);
+        model.addAttribute("time", LocalTime.now());
+        return "game-content";
     }
 }
