4aa24275c7097db862706876b7c56c5762405e2b
Thomas Dreja <thomas.dreja@gmail.com>
2025-01-18 23:28:33 +0100

    chore(Test): Integrate Junit output to typescript

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,17 @@ module.exports = {
   transform: {
     "^.+.tsx?$": ["ts-jest",{}],
   },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.[t]sx?$",
+  reporters: [
+    'default',
+    ['jest-junit', {
+      outputDirectory: 'build/test-results/typescript', 
+      outputName: 'report.xml', 
+      includeConsoleOutput: true, 
+      reportTestSuiteErrors: true, 
+      classNameTemplate: '{title}',
+      titleTemplate: '{title}',
+      suiteNameTemplate: '{filename}'
+    }]
+  ]
 };
